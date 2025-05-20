abcc4e69917b69df99c95a47557e602a5b3a5533
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2025-05-20 23:42:32 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Carosello from "./components/Carosell";
+import Search from "./components/search";
+import ArticlePage from "./page/ArticlePage.jsx";
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container" style={{ paddingTop: "110px" }}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1
+                  style={{
+                    color: "black",
+                    fontFamily: "Playfair Display, serif",
+                    fontSize: "30px",
+                  }}
+                >
+                  The New York Times
+                </h1>
+                <hr />
+                <div className="ros">
+                  <Carosello />
+                  <div>
+                    <HomePage />
+                  </div>
+                </div>
+                <div className="contr">
+                  <Search />
+                </div>
+              </>
+            }
+          />
+          <Route path="/article/:id" element={<ArticlePage />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/features/articles/ articlesSlice.js
@@ -0,0 +1,60 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { fetchArticlesAPI } from "../../api/apiClient";
+
+// Thunk asincrono con axios
+export const fetchArticles = createAsyncThunk(
+  "articles/fetchArticles",
+  async (query, thunkAPI) => {
+    try {
+      const response = await fetchArticlesAPI(query);
+      const data = response.data;
+      return data.response.docs.map((doc) => ({
+        id: doc._id,
+        title: doc.headline?.main ?? "Nessun titolo trovato",
+        url: doc.web_url,
+        multimedia: doc.multimedia,
+        snippet: doc.snippet,
+      }));
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Errore nella ricerca articoli.");
+    }
+  }
+);
+
+const articlesSlice = createSlice({
+  name: "articles",
+  initialState: {
+    query: "",
+    articles: [],
+    loading: false,
+    error: null,
+    selectedArticle: null,
+  },
+  reducers: {
+    setQuery(state, action) {
+      state.query = action.payload;
+    },
+    setSelectedArticle(state, action) {
+      state.selectedArticle = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchArticles.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchArticles.fulfilled, (state, action) => {
+        state.loading = false;
+        state.articles = action.payload;
+      })
+      .addCase(fetchArticles.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Errore durante il recupero dei dati.";
+      });
+  },
+});
+
+export const { setQuery, setSelectedArticle } = articlesSlice.actions;
+export default articlesSlice.reducer;
